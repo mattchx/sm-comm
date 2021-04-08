546ade1c985054a6fea222b2edc3752e23c53f9c
Matt <mattcharlesh@gmail.com>
2021-04-08 15:56:38 -0400

    moved Header to App.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import PageForm from '../components/PageForm'
-import Header from '../components/Header'
 
 function Home() {
     return (
         <div>
-            <Header/>
             <PageForm/>
         </div>
     )
