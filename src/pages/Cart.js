ef5b12ebdd9b0854dc1887d6161a70e53b7ef80c
Matt <mattcharlesh@gmail.com>
2021-04-08 22:29:51 -0400

    Added reduce to calculate total due

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -7,11 +7,14 @@ const itemsInCart = useSelector((state) => state.cart);
  console.log(itemsInCart) 
 return (
     <Grid container direction="column" alignItems="center" justify="center">
-        <h5>This is your shopping cart</h5>
+        <h3>This is your shopping cart</h3>
     <Card>
       
       {itemsInCart &&
         itemsInCart.items.map((item) => <p key={item.id}>{item.title}</p>)}
+        <h4>Total due: ${itemsInCart.items.reduce((acc, next) => {
+        return acc + parseInt(next.price)
+        },0)}</h4>
     </Card>
     </Grid>
   );
