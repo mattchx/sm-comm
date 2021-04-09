1bca946dbd61b4f2b876c98eead908a79f32b222
Matt <mattcharlesh@gmail.com>
2021-04-08 22:33:21 -0400

    formatting correction

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import {Card, Grid} from '@material-ui/core'
+import { Card, Grid } from '@material-ui/core';
 function Cart() {
-
-const itemsInCart = useSelector((state) => state.cart);
- console.log(itemsInCart) 
-return (
+  const itemsInCart = useSelector((state) => state.cart);
+  console.log(itemsInCart);
+  return (
     <Grid container direction="column" alignItems="center" justify="center">
-        <h3>This is your shopping cart</h3>
-    <Card>
-      
-      {itemsInCart &&
-        itemsInCart.items.map((item) => <p key={item.id}>{item.title}</p>)}
-        <h4>Total due: ${itemsInCart.items.reduce((acc, next) => {
-        return acc + parseInt(next.price)
-        },0)}</h4>
-    </Card>
+      <h3>This is your shopping cart</h3>
+      <Card>
+        {itemsInCart &&
+          itemsInCart.items.map((item) => <p key={item.id}>{item.title}</p>)}
+        <h4>
+          Total due: $
+          {itemsInCart.items.reduce((acc, next) => {
+            return acc + parseInt(next.price);
+          }, 0)}
+        </h4>
+      </Card>
     </Grid>
   );
 }
--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -5,10 +5,9 @@ items: [],
 totalItems: 0
 };
 
-export default function postsReducer(state = initialState, action) {
+export default function cartReducer(state = initialState, action) {
   switch (action.type) {
     case ADD_TO_CART:
-        console.log(state)
       return { ...state, items:[...state.items, action.payload] };
     case REMOVE_FROM_CART:
       return { posts: action.payload, loading: false, hasErrors: false };
